23cdf5c92fbf7b2be0c29293651280b7f93c741d
tahereh <tahereh.moeini@gmail.com>
2023-01-24 23:57:36 +0330

    persian admin page

--- web/admin.py
+++ web/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Expense,Income,Token
 
 # Register your models here.
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display=('text','amount','date')
+    search_fields=('text',)
+    list_filter=(['date'])
+
+admin.site.register(Expense,ExpenseAdmin)
 admin.site.register(Income)
 admin.site.register(Token)
--- web/models.py
+++ web/models.py
@@ -9,12 +9,18 @@ class Token(models.Model):
         return "{}-token".format(self.user)
 
 class Expense(models.Model):
-    text=models.CharField(max_length=255)
-    date=models.DateTimeField()
-    amount=models.BigIntegerField()
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    text=models.CharField(max_length=255,verbose_name="متن")
+    date=models.DateTimeField(verbose_name="تاریخ")
+    amount=models.BigIntegerField(verbose_name="مقدار")
+    user=models.ForeignKey(User, on_delete=models.CASCADE,verbose_name="کاربر")
+    class Meta:
+        verbose_name="خرج"
+        verbose_name_plural="خرج ها"
+        ordering=['date']
     def __str__(self):
         return "{}-{}".format(self.date,self.amount)
+    
+            
 class Income(models.Model):
     text=models.CharField(max_length=255)
     date=models.DateTimeField()
@@ -22,3 +28,6 @@ class Income(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return "{}-{}".format(self.date,self.amount)
+    class Meta:
+        verbose_name="در آمد"
+        verbose_name_plural="درآمدها"   
